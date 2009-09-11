19bb35526b406c6caeb137d5919ed9199ec9f3df
Nowhere Man <nowhereman@office>
2009-09-11 14:20:44 +0200

    Adding routes, so URLs looks better !

--- routes.rb
+++ routes.rb
@@ -1,1 +1,14 @@
 #custom routes for this plugin
+ActionController::Routing::Routes.draw do |map|
+  map.with_options :controller => 'todos' do |todos_routes|
+    todos_routes.connect "projects/:project_id/todos/:action/:id"
+    todos_routes.connect "projects/:project_id/todos/:action/:id.:format"
+    todos_routes.connect 'projects/:project_id/issues/:issue_id/todos/:parent_id/new', :action => 'new'
+    todos_routes.connect 'projects/:project_id/issues/:issue_id/todos/:parent_id/new.:format', :action => 'new'
+  end
+
+  map.with_options :controller => 'mytodos' do |mytodos_routes|
+    mytodos_routes.connect 'mytodos/:parent_id/new', :action => 'new'
+    mytodos_routes.connect 'mytodos/:parent_id/new.:format', :action => 'new'
+  end
+end
