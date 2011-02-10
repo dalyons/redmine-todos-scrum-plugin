06cfd76b94c7c71b63ecab4c2e520bbfa27357ca
David Lyons <dalyons@gmail.com>
2011-02-10 13:58:00 +1100

    bump version

--- init.rb
+++ init.rb
@@ -34,7 +34,7 @@ Redmine::Plugin.register :redmine_todos_plugin do
   name 'Redmine Todo Lists plugin'
   author 'David Lyons'
   description 'A plugin to create and manage agile-esque todo lists on a per project basis.'
-  version '0.0.4.1'
+  version '0.0.4.3'
   
 
   settings :default => {
