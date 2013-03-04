b7b04edd1020d223844a734a33eac8283d7004e5
suhas kodgire <suhas.kodgire@gslab.com>
2013-03-04 21:35:40 +0530

    My Todos fuctionality completed and addding related files for the same

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
   
-match 'my/todos', :to => 'mytodos#index'
-
+match  'my/todos'                                            => 'mytodos#index'
+post   '/users/:user_id/todos'                               => 'mytodos#create'
+post   '/users/:user_id/todos/:id/toggle_complete'           => 'mytodos#toggle_complete'
+delete '/users/:user_id/todos/:id'                           => 'mytodos#destroy'
+get    '/users/:user_id/todos/:id/edit'                      => 'mytodos#edit'
+put    '/users/:user_id/todos/:id'                           => 'mytodos#update'
+get    '/users/:user_id/todos/new'                           => 'mytodos#new'
+get    '/users/:user_id/todos/:id'                           => 'mytodos#show'
+get    '/projects/:project_id/todos/reload'                  => 'todos#show'
 #resources :todos do
 #  scope '/projects/:project_id', :name_prefix => 'project_' do
 #    collection do
